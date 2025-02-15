82535d3eec9ad63405fe3c12c692c692e4b822fe
asdzxcvbn1509a <141426904+asdzxcvbn1509a@users.noreply.github.com>
2025-02-15 22:28:11 +0700

    Update Area.cpp

--- Area.cpp
+++ Area.cpp
@@ -5,24 +5,46 @@ using namespace std;
 int main() {
     class area {
         public:
-        
         void square() {
             double side;
             cout << endl << "Square" << endl;
             cout << "Enter number of side : ";
             cin >> side;
             cout << "The area of square = " << side*side << endl;
-            cout << "The perimeter of square = " << side*4 << endl;
+        }
+        void rectancle() {
+            double width, length;
+            cout << endl << "Rectancle" << endl;
+            cout << "Enter number of width : ";
+            cin >> width;
+            cout << "Enter number of length : ";
+            cin >> length;
+            cout << "The area of rectancle = " << width*length;
+        }
+        void triangle() {
+            double base, height;
+            cout << endl << "Triangle" << endl;
+            cout << "Enter number of base : ";
+            cin >> base;
+            cout << "Enter number of height : ";
+            cin >> height;
+            cout << "The area of triangle = " << 0.5*base*height;
+        }
+        void rhombus() {
+            double base, height;
+            cout << endl << "Rhombus" << endl;
+            cout << "Enter number of base : ";
+            cin >> base;
+            cout << "Enter number of height : ";
+            cin >> height;
+            cout << "The area of rhombus = " << base*height;
         }
 };
     cout << "1 = Square" << endl;
     cout << "2 = Rectancle" << endl;
     cout << "3 = Triangle" << endl;
-    cout << "4 = rhombus" << endl;
-    cout << "5 = Parallelogram" << endl;
-    cout << "6 = Kite-shaped rectangular" << endl;
-    cout << "7 = trapezium" << endl;
-    
+    cout << "4 = Rhombus" << endl;
+
     int choice;
     cout << endl << "Enter the number : ";
     cin >> choice;
@@ -32,22 +54,14 @@ int main() {
         a.square();
     }
     if (choice == 2) {
-        
+        a.rectancle();
     }
     if (choice == 3) {
-        
+        a.triangle();
     }
     if (choice == 4) {
-        
+        a.rhombus();
     }
-    if (choice == 5) {
-        
-    }
-    if (choice == 6) {
-        
-    }
-    if (choice == 7) {
-        
-    }
+    
     return 0;
 }
