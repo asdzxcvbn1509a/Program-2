bffc5076135b5e5ce24a243a818656ec0e465181
asdzxcvbn1509a <141426904+asdzxcvbn1509a@users.noreply.github.com>
2025-02-16 19:30:59 +0700

    Create Red_blue.cpp

---
+++ Red_blue.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Permutation with Repetition" << endl;
+    int red, blue, sum, red_fac=1, blue_fac=1, sum_fac=1, result;
+    cout << "Enter the number of red ball : ";
+    cin >> red; 
+    cout << "Enter the number of blue ball : ";
+    cin >> blue;
+    sum = red+blue;
+    
+    for (int i=1; i<=sum; i++) {
+        sum_fac = sum_fac*i;
+    }
+    for (int i=1; i<=red; i++) {
+        red_fac = red_fac*i;
+    }
+    for (int i=1; i<=blue; i++) {
+        blue_fac = blue_fac*i;
+    }
+    result = sum_fac/(red_fac*blue_fac);//(a+b)!/(a!*b!)
+    cout << "Number of possibilities for arranging balls : "<< result << " ways";
+    return 0;
+}
